cebd9fb7dac1476af7b5b26b2795ab1afc0ec4ba
ThaCuber <70547062+ThaCuber@users.noreply.github.com>
2023-08-10 09:47:10 -0400

    oh shit, the go back button doesn't work

--- things/fantroll/fantroll.js
+++ things/fantroll/fantroll.js
@@ -6,7 +6,7 @@ function randomColor() {
 // this is probably hacky as hell but I don't know how else can I do this so
 let back = document.createElement("a");
 back.text = "Go back home";
-back.href = "../index.html";
+back.href = "https://thacuber.github.io/";
 document.querySelector("body").appendChild(back);
 
 let canvas = document.getElementById("troll-canvas");
